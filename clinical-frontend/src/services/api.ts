51fbb60a3a18d9662ddf46b4501f97521438728e
sorimdevs-tech <sorimdevs@gmail.com>
2026-01-23 17:15:11 +0530

    Update api.ts

--- clinical-frontend/src/services/api.ts
+++ clinical-frontend/src/services/api.ts
@@ -1,5 +1,5 @@
 // const API_BASE_URL = "http://localhost:8000";
-const API_BASE_URL = "https://wellnessdev.onrender.com";
+const API_BASE_URL = "http://localhost:8000";
  
 export interface ApiResponse<T> {
   data?: T;
